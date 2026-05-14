1aaeaaeff52c3fb212a73c459b79cae4cc3d50cd
343111274 <343111274@C-2JM7GB2.tdsb.on.ca>
2026-05-14 11:09:55 -0400

    2ndcommitfan

--- src/MisinformationCase.java
+++ src/MisinformationCase.java
@@ -7,6 +7,18 @@
  *
  * @author 345341119
  */
-public class MisinformationCase {
+public class MisinformationCase extends EthicsCase {
+    private String mediaType;
+    public MisinformationCase(String caseTitle, String description, String mediaType){
+        super(caseTitle, description, "Misinformation");
+        this.mediaType= mediaType;
+    }
+    public String getmediaType() {
+        return mediaType; 
+    }
     
+    @Override
+    public String toString(){
+        return super.toString() + "\nMedia Type: " + mediaType;
+    }
 }

6692b59344e0d16c92ce3975c9d69dd9c93b8fb8
fanfanuwu <343111274@tdsb.ca>
2026-05-14 11:15:23 -0400

    Update AlgorithmCase.java

--- src/AlgorithmCase.java
+++ src/AlgorithmCase.java
@@ -7,6 +7,15 @@
  *
  * @author 345341119
  */
-public class AlgorithmCase {
-    
+public class AlgorithmCase extends EthicsCase {
+    private String biasType;
+    public AlgorithmCase(String caseTitle, String description, String biasType){
+        super(caseTitle, description, "Algorithm");
+        this.biasType = biasType;
+    }
+    public String getbiasType(){ return biasType; }
+    @Override
+    public String toString(){
+        return super.toString() + "\nBias Type: " + biasType;
+    }
 }

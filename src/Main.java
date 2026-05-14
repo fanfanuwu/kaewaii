cb44bfc53f6488cbc86b72ec642f39acb033485c
345341119 <345341119@C-FWVCGK2.tdsb.on.ca>
2026-05-14 11:08:54 -0400

    2nd commit - kae

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 345341119
+ */
+public class Main {
+    public static void main(String args[]) {
+        PrivacyCase privacyCase = new PrivacyCase("Data Leak Case","Company exposed user emails publicly.","Personal Data.");
+    }
+}
